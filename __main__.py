c303cbd5d19a632df78b0509c15d95c0260f4146
Justin Murdock <justin.murdock96@gmail.com>
2015-11-16 09:00:46 -0800

    multithreading in python

---
+++ __main__.py
@@ -0,0 +1,31 @@
+import threading
+from queue import Queue
+import time
+
+print_lock = threading.Lock()
+
+q = Queue()
+def exampleJob(worker):
+    time.sleep(0.5)
+    with print_lock:
+        print(threading.current_thread().name, worker)
+        
+def threader():
+    while True:
+        worker = q.get()
+        exampleJob(worker)
+        q.task_done()
+
+for x in range(10 ):
+    t = threading.Thread(target = threader)
+    t.daemon = True
+    t.start()
+
+start = time.time()
+
+for worker in range(20):
+    q.put(worker)
+    
+q.join()
+
+print('entire job took: '+str(time.time()-start) + "s")
